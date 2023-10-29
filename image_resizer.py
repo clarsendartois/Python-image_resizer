a9dbafe5ca9ca317c20549817419d01516468580
Clarsen D'Artois <116375601+clarsendartois@users.noreply.github.com>
2023-10-28 23:43:17 -0400

    modified, new file

--- image_resizer.py
+++ image_resizer.py
@@ -1,8 +1,8 @@
-# Use your Terminal, if you are on 
+# Use your Terminal, if you are on
 # Windows type: python image_resizer.py
 from PIL import Image
 
 img = Image.open("./img/img.png")
-img.thumbnail((1080, 1080))
+img.thumbnail((250, 250))
 img.save("./img_resize/img_resize.png")
 print(img.size)
